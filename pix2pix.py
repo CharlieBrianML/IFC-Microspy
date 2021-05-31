da2b6cb9ff255fa7f285f6a709440fe84f5354ae
CharlieML <charls_brian@hotmail.es>
2021-05-31 14:59:08 -0500

    Se ha agregado archivo pix2pix.py

---
+++ pix2pix.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# ## ###############################################
+#
+# osmarque.py
+# Archivo principal
+#
+# Autor: Charlie Brian Monterrubio Lopez
+# License: MIT
+#
+# ## ###############################################
