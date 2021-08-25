d59ad437466b5901479bf8330ed73fde068167b8
CharlieML <charls_brian@hotmail.es>
2021-08-25 14:13:07 -0500

    Se ha agregado documentacion al archivo imageFunctions.py

--- imageFunctions.py
+++ imageFunctions.py
@@ -3,7 +3,7 @@
 # ## ###############################################
 #
 # imageFunctions.py
-# Contiene todas las herramientas para el procesamiento de imagenes
+# Contains all the tools for image processing
 #
 # Autor: Charlie Brian Monterrubio Lopez
 # License: MIT
@@ -17,34 +17,36 @@ import cv2
 
 
 def normalizar(data):
+	"""Normalizes a data matrix"""
 	max=np.amax(data)#Se calcula el valor maximo del vector
 	for p in range(data.shape[0]):
 		for m in range(data.shape[1]):
 			data[p][m]=(data[p][m]*256)/max  #Formula para normalizar los valores de [0, 255]
 	return data
 
-#Implementa la funcion rescale_intensity de skimage
 def rescaleSkimage(img):
+	"""Implement the function rescale_intensity from skimage"""
 	imgRescale = rescale_intensity(img, in_range=(0, 255))
 	imgRescale = (imgRescale * 255).astype("uint8")
 	return imgRescale
 
 def mostrarImagen(nameWindow, img, close):
+	"""Display an image"""
 	cv2.imshow(nameWindow, img)
 	if (close):
 		cv2.waitKey(0)
 		cv2.destroyAllWindows()
 	
 def guardarImagen(nameFile, img):
+	"""Save an image"""
 	cv2.imwrite(nameFile, img)
 	
-# def imgReadCv2(nameImg,channel):
-	# return cv2.imread(nameImg,channel)
-	
 def escalaGrises(img):
+	"""Transforms an image to grayscale"""
 	return cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-#	
+	
 def denoisingTV(img,value):
+	"""Applies the total variation filter to an image"""
 	return denoise_tv_chambolle(img, weight=value)
 	
 def imgReadCv2(nameImg):
@@ -53,6 +55,7 @@ def imgReadCv2(nameImg):
 def validatePSF(tiff,psf):
 	if(tiff.shape==psf.shape):
 		return True
+		
 def istiffRGB(tiff):
 	if(tiff[len(tiff)-1]==3):
 		return True
