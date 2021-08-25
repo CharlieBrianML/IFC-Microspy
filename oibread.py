430f7ad42f0b50be963c8144d738d324261a0c78
CharlieML <charls_brian@hotmail.es>
2021-08-25 14:14:02 -0500

    Se ha agregado documentacion al archivo oibread.py

--- oibread.py
+++ oibread.py
@@ -3,7 +3,7 @@
 # ## ###############################################
 #
 # oibread.py
-# Contiene las herramientas para procesar un archivo .oib
+# Contains the tools to process an .oib file
 #
 # Autor: Charlie Brian Monterrubio Lopez
 # License: MIT
@@ -13,12 +13,13 @@
 from oiffile import *
 
 def getMetadata(filename):
+	"""Gets the metadata of an .oib file"""
 	with OifFile(filename) as oib:
 		metadata = oib.mainfile
 		metadata.update(dict(axes=oib.axes,shape=oib.shape,dtype=oib.dtype, num_aperture=1.35, pinhole_radius=(120000/1000)/2, magnification=0.75, refr_index=1.45))
 	return metadata
 	
-#Obtencion de la matriz del oib	
 def get_matrix_oib(filename):
+	"""Gets the array associated with the .oib file"""
 	matrix = imread(filename)
 	return matrix
