138fae97f951b983a75db8ee5cb9780364804360
CharlieBrianML <charls_brian@hotmail.es>
2021-08-30 17:02:56 -0500

    Limpieza de codigo en sr.py

--- sr.py
+++ sr.py
@@ -2,8 +2,8 @@
 # -*- coding: utf-8 -*-
 # ## ###############################################
 #
-# ifc_srm.py
-# File main
+# sr.py
+# Neural Network
 #
 # Autor: Charlie Brian Monterrubio Lopez
 # License: MIT
@@ -72,20 +72,14 @@ def downscale_image(image):
 	
 def model_NeuralNetwork(image_path):
 	global model
-	#SAVED_MODEL_PATH = "https://tfhub.dev/captain-pool/esrgan-tf2/1"
 	hr_image = preprocess_image(image_path)
 	lr_image = downscale_image(tf.squeeze(hr_image))
-	#model = hub.load(SAVED_MODEL_PATH)
-
-	# start = time.time()
+	
 	# fake_image = model(hr_image)
 	# fake_image = tf.squeeze(fake_image)
-	# print("Time Taken: %f" % (time.time() - start))
 
-	start = time.time()
 	fake_image = model(lr_image)
 	fake_image = tf.squeeze(fake_image)
-	print("Time Taken: %f" % (time.time() - start))
 	return fake_image, hr_image
 	
 		
@@ -139,112 +133,27 @@ def nn(dev_img, img_tensor):
 	
 	info = np.load('info.npy', allow_pickle=True).item()
 	img_output = np.zeros((info['z'], info['c'], info['x'], info['y']))
-	'''
-	print('imagen a guardar: ', dev_img.shape)
-	tifffile.imsave('training_deconv/image.tif', dev_img, imagej=True)					
-	'''
+
 	imgs_deconv = os.listdir('training_deconv')
 	
-	# Declaring Constants
-	#IMAGE_PATH = "training_deconv/1_6.jpg"
+	# Path model 
 	SAVED_MODEL_PATH = "https://tfhub.dev/captain-pool/esrgan-tf2/1"
 
-	#imgdeconv=cv2.imread(IMAGE_PATH,0)
-	#img=cv2.imread('training_set/1_6.jpg',0)
-	'''
-	from skimage import io
-	img = io.imread('training_deconv/image.tif')
-	
-	print('imagen leida: ', img.shape)
-	'''
-	# hr_image = preprocess_image(IMAGE_PATH)
-	# lr_image = downscale_image(tf.squeeze(hr_image))
-
-	# Plotting Low Resolution Image
-	#plot_image(tf.squeeze(lr_image), title="Low Resolution")
-
-	# Plotting Original Resolution image
-	#plot_image(tf.squeeze(hr_image), title="Original Image")
-	#save_image(tf.squeeze(hr_image), filename="Original Image")	
-
 	model = hub.load(SAVED_MODEL_PATH)
-
-	# # start = time.time()
-	# # fake_image = model(hr_image)
-	# # fake_image = tf.squeeze(fake_image)
-	# # print("Time Taken: %f" % (time.time() - start))
-
-	# start = time.time()
-	# fake_image = model(lr_image)
-	# fake_image = tf.squeeze(fake_image)
-	# print("Time Taken: %f" % (time.time() - start))
-
-	# # Plotting Super Resolution Image
-	# plot_image(tf.squeeze(fake_image), title="Super Resolution")
-	# save_image(tf.squeeze(fake_image), filename="Super Resolution")
 	
 	if not(os.path.isdir('output_NN')):
 		os.mkdir('output_NN')	
 
+	start = time.time()
 	for image in imgs_deconv:
-		print('Processing: ', image)
 		fake_image, hr_image = model_NeuralNetwork('training_deconv/'+image)
 		img_array = tensor_to_array(tf.squeeze(fake_image))
 		plt.imsave("output_NN/neural_network_"+image.split('.')[0]+'.png', img_array, cmap='gray')
-		print(type(np.asarray(img_array)))
 		c = int(image.split('.')[0].split('_')[0])-1
 		z = int(image.split('.')[0].split('_')[1])-1
-		print('Channel; ', c)
-		print('Z; ', z)
-		#img_output[z,c,:,:] = cv2.merge(np.asarray(img_array))
+		print('Processing: ', image, '\tc: ',c,'z: ',z)
 		img_output[z,c,:,:] = np.asarray(img_array)[:,:,0]
-		#cv2.imwrite("output_NN/neural_network_"+image.split('.')[0]+'.png', tensor_to_array(tf.squeeze(fake_image)))
-	'''
-	for c in range(img.shape[3]):
-		for z in range(img.shape[0]):
-			print('Processing channel '+str(c)+' z: ', str(z))
-			fake_image, hr_image = model_NeuralNetwork(img)
-			img_array = tensor_to_array(tf.squeeze(fake_image))
-			plt.imsave("output_NN/neural_network_"+image.split('.')[0]+'.png', img_array, cmap='gray')
-			print(type(np.asarray(img_array)))
-			img_output[] = np.asarray(img_array)
-	'''	
-	#nnimg = it.NewWindow('Neural Network '+it.file.split('/')[len(it.file.split('/'))-1])
-	#nnimg.desplay_image('Neural Network '+it.file.split('/')[len(it.file.split('/'))-1], )	
-	import interfaceTools as it
-	nnimg = it.NewWindow('Neural Network ')
-	nnimg.desplay_image('Neural Network ', img_output)			
+	print("Time Taken: %f" % (time.time() - start))	
 	
-	#fake_image, hr_image = model_NeuralNetwork(IMAGE_PATH)
-
-	# plot_image(tf.squeeze(fake_image), title="Neural Network")
-	# # Calculating PSNR wrt Original Image
-	# psnr = tf.image.psnr(tf.clip_by_value(fake_image, 0, 255),tf.clip_by_value(hr_image, 0, 255), max_val=255)
-	# print("PSNR Achieved: %f" % psnr)
-
-	c=1
-	z=0
-
-	# plt.rcParams['figure.figsize'] = [15, 10]
-	# fig, axes = plt.subplots(1, 3)
-	# fig.tight_layout()
-	# plt.subplot(131)
-	# #plt.imshow(img_tensor[z,c,:,:], cmap='gray')
-	# plt.imshow(img, cmap='gray')
-	# plt.axis("off")
-	# plt.title('Original')
-	# #plot_image(img_tensor[z,c,:,:], title="Original")
-	# plt.subplot(132)
-	# save_image(tf.squeeze(hr_image), filename='output_NN/SuperResolution')
-	# fig.tight_layout()
-	# #plt.imshow(dev_img[c,z,:,:], cmap='gray')
-	# plt.imshow(imgdeconv, cmap='gray')
-	# plt.axis("off")
-	# plt.title('Deconvolution')	
-	# #plot_image(dev_img[c,z,:,:], "Deconvolution")
-	# plt.subplot(133)
-	# fig.tight_layout()
-	# plot_image(tf.squeeze(fake_image), "Neural Network")
-	# plt.savefig("output_NN/NeuralNetwork_c"+str(c)+"_z"+str(z)+".jpg", bbox_inches="tight")
-	# print("PSNR: %f" % psnr)
-	# plt.show()
+	nnimg = it.NewWindow('Neural Network ')
+	nnimg.desplay_image('Neural Network ', img_output)
