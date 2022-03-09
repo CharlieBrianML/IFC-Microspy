9786d20259a4620e363c7073646a6229a50a8c70
Charlie Monterrubio <cbmonterrubio@minsait.com>
2022-03-09 12:37:46 -0600

    Integracion de indices dinamicos, diccionario metadata y procesamiento de canal t

--- src/esrgan.py
+++ src/esrgan.py
@@ -23,6 +23,9 @@ import src.interfaceTools as it
 #os.environ["TFHUB_DOWNLOAD_PROGRESS"] = "True"
 
 model = None
+inx0, inx1, inx2, inx3 = (None, None, None, None)
+inx0p, inx1p, inx2p, inx3p = (None, None, None, None)
+metadata = {}
 
 def preprocess_image(image_path):
 	""" Loads image from path and preprocesses to make it model ready
@@ -105,7 +108,7 @@ def tensor_to_array(image):
 	image = Image.fromarray(tf.cast(image, tf.uint8).numpy())	
 	return image
 
-def nn(img_tensor, index):
+def nn(img_tensor, index, metadataimg):
 	import tifffile
 	import src.interfaceTools as it
 	from shutil import rmtree
@@ -114,17 +117,24 @@ def nn(img_tensor, index):
 	import os
 	global model
 	
+	global inx0, inx1, inx2, inx3, inx0p, inx1p, inx2p, inx3p, metadata
+	inx0, inx1, inx2, inx3 = (None, None, None, None)
+	inx0p, inx1p, inx2p, inx3p = (None, None, None, None)
+	
+	metadata = metadataimg
+	
 	if(len(it.windows_img)>0):
 	
 		if not(os.path.isdir('src/cache/training_set')):
 			os.mkdir('src/cache/training_set')
 			
-		img_path = 'src/cache/training_set/training_'+it.windows_img[index].nameWindow
-	
-		# Creation of the training set
-		#if not(os.path.isdir(img_path)):
-		os.mkdir(img_path)
+		# img_path = 'src/cache/training_set/training_'+it.windows_img[index].nameWindow.replace(" ", "").replace("-", "")
+		img_path = 'src/cache/training_set/'
+		
 		print(img_tensor.shape)
+		
+		if (it.windows_img[index].metadata['type']=='uint16'):
+			img_tensor = np.uint16(img_tensor)*(255/4095)
 		
 		if (img_tensor.ndim == 2):
 			img = np.zeros((img_tensor.shape[0],img_tensor.shape[1],3))
@@ -135,29 +145,61 @@ def nn(img_tensor, index):
 		if (img_tensor.ndim == 3):
 			if (istiffRGB(img_tensor.shape)):
 				cv2.imwrite(img_path+'/1.jpg', img_tensor)
-			elif (img_tensor.shape[0]>4):
-				img = np.zeros((img_tensor.shape[1],img_tensor.shape[2],3))
-				for z in range(img_tensor.shape[0]):
-					img[:,:,0] = np.uint8(img_tensor[z,:,:])
-					img[:,:,1] = np.uint8(img_tensor[z,:,:])
-					img[:,:,2] = np.uint8(img_tensor[z,:,:])			
+			elif ('slices' in metadata):
+				img = np.zeros((metadata['X'],metadata['Y'],3))
+				for z in range(metadata['slices']['value']):
+					updateIndex(metadata['slices']['index'], z)
+					img[:,:,0] = np.uint8(img_tensor[inx0p:inx0,inx1p:inx1,inx2p:inx2])
+					img[:,:,1] = np.uint8(img_tensor[inx0p:inx0,inx1p:inx1,inx2p:inx2])
+					img[:,:,2] = np.uint8(img_tensor[inx0p:inx0,inx1p:inx1,inx2p:inx2])
 					cv2.imwrite(img_path+'/'+str(z+1)+'.jpg', img)
+			elif ('frames' in metadata):
+				img = np.zeros((metadata['X'],metadata['Y'],3))
+				for f in range(metadata['frames']['value']):
+					updateIndex(metadata['frames']['index'], f)
+					img[:,:,0] = np.uint8(img_tensor[inx0p:inx0,inx1p:inx1,inx2p:inx2])
+					img[:,:,1] = np.uint8(img_tensor[inx0p:inx0,inx1p:inx1,inx2p:inx2])
+					img[:,:,2] = np.uint8(img_tensor[inx0p:inx0,inx1p:inx1,inx2p:inx2])
+					cv2.imwrite(str(f+1)+'.jpg', img)		
+					cv2.imwrite(img_path+'/'+str(f+1)+'.jpg', img)	
 			else:
-				img = np.zeros((img_tensor.shape[1],img_tensor.shape[2],3))
-				for c in range(img_tensor.shape[0]):
-					img[:,:,0] = np.uint8(img_tensor[c,:,:])
-					img[:,:,1] = np.uint8(img_tensor[c,:,:])
-					img[:,:,2] = np.uint8(img_tensor[c,:,:])			
+				img = np.zeros((metadata['X'],metadata['Y'],3))
+				for c in range(metadata['channels']['value']):
+					updateIndex(metadata['channels']['index'], c)
+					img[:,:,0] = np.uint8(img_tensor[inx0p:inx0,inx1p:inx1,inx2p:inx2])
+					img[:,:,1] = np.uint8(img_tensor[inx0p:inx0,inx1p:inx1,inx2p:inx2])
+					img[:,:,2] = np.uint8(img_tensor[inx0p:inx0,inx1p:inx1,inx2p:inx2])
 					cv2.imwrite(img_path+'/'+str(c+1)+'.jpg', img)			
 				
 		if (img_tensor.ndim == 4):
-			img = np.zeros((img_tensor.shape[2],img_tensor.shape[3],3))	
-			for c in range(img_tensor.shape[1]):
-				for z in range(img_tensor.shape[0]):
-					img[:,:,0] = np.uint8(img_tensor[z,c,:,:])
-					img[:,:,1] = np.uint8(img_tensor[z,c,:,:])
-					img[:,:,2] = np.uint8(img_tensor[z,c,:,:])
-					cv2.imwrite(img_path+'/'+str(c+1)+'_'+str(z+1)+'.jpg', img)
+			img = np.zeros((metadata['X'],metadata['Y'],3))
+			if(('channels' in metadata) and ('slices' in metadata)):
+				for c in range(metadata['channels']['value']):
+					updateIndex(metadata['channels']['index'], c)
+					for z in range(metadata['slices']['value']):
+						updateIndex(metadata['slices']['index'], z)
+						img[:,:,0] = np.uint8(img_tensor[inx0p:inx0,inx1p:inx1,inx2p:inx2,inx3p:inx3])
+						img[:,:,1] = np.uint8(img_tensor[inx0p:inx0,inx1p:inx1,inx2p:inx2,inx3p:inx3])
+						img[:,:,2] = np.uint8(img_tensor[inx0p:inx0,inx1p:inx1,inx2p:inx2,inx3p:inx3])
+						cv2.imwrite(img_path+'/'+str(c+1)+'_'+str(z+1)+'.jpg', img)
+			if(('channels' in metadata) and ('frames' in metadata)):
+				for c in range(metadata['channels']['value']):
+					updateIndex(metadata['channels']['index'], c)
+					for f in range(metadata['frames']['value']):
+						updateIndex(metadata['frames']['index'], f)
+						img[:,:,0] = np.uint8(img_tensor[inx0p:inx0,inx1p:inx1,inx2p:inx2,inx3p:inx3])
+						img[:,:,1] = np.uint8(img_tensor[inx0p:inx0,inx1p:inx1,inx2p:inx2,inx3p:inx3])
+						img[:,:,2] = np.uint8(img_tensor[inx0p:inx0,inx1p:inx1,inx2p:inx2,inx3p:inx3])
+						cv2.imwrite(img_path+'/'+str(c+1)+'_'+str(f+1)+'.jpg', img)
+			if(('frames' in metadata) and ('slices' in metadata)):
+				for f in range(metadata['frames']['value']):
+					updateIndex(metadata['frames']['index'], f)
+					for z in range(metadata['slices']['value']):
+						updateIndex(metadata['slices']['index'], z)
+						img[:,:,0] = np.uint8(img_tensor[inx0p:inx0,inx1p:inx1,inx2p:inx2,inx3p:inx3].reshape((metadata['X'],metadata['Y'])) )
+						img[:,:,1] = np.uint8(img_tensor[inx0p:inx0,inx1p:inx1,inx2p:inx2,inx3p:inx3].reshape((metadata['X'],metadata['Y'])) )
+						img[:,:,2] = np.uint8(img_tensor[inx0p:inx0,inx1p:inx1,inx2p:inx2,inx3p:inx3].reshape((metadata['X'],metadata['Y'])) )
+						cv2.imwrite(img_path+'/'+str(f+1)+'_'+str(z+1)+'.jpg', img)					
 		
 		img_output = np.zeros(img_tensor.shape)
 		imgs = os.listdir(img_path)
@@ -184,36 +226,72 @@ def nn(img_tensor, index):
 					img_array = tensor_to_array(tf.squeeze(fake_image))
 					img_output = np.asarray(img_array)
 					display1f = True
-				elif (img_output.shape[0]>4):
+				elif ('slices' in metadata):
 					for image in imgs:
 						z = int(image.split('.')[0])-1
 						it.printMessage('Processing: '+image+'\tz: '+str(z))
 						fake_image, hr_image = model_NeuralNetwork(img_path+'/'+image)
 						img_array = tensor_to_array(tf.squeeze(fake_image))
-						img_output[z,:,:] = np.asarray(img_array)[:,:,0]				
+						updateIndex(metadata['slices']['index'], z)
+						img_output[inx0p:inx0,inx1p:inx1,inx2p:inx2] = np.asarray(img_array)[:,:,0]
+				elif ('frames' in metadata):
+					for image in imgs:
+						f = int(image.split('.')[0])-1
+						it.printMessage('Processing: '+image+'\tf: '+str(f))
+						fake_image, hr_image = model_NeuralNetwork(img_path+'/'+image)
+						img_array = tensor_to_array(tf.squeeze(fake_image))
+						updateIndex(metadata['frames']['index'], f)
+						img_output[inx0p:inx0,inx1p:inx1,inx2p:inx2] = np.asarray(img_array)[:,:,0]						
 				else:
 					for image in imgs:
 						c = int(image.split('.')[0])-1
 						it.printMessage('Processing: '+image+'\tc: '+str(c))		
 						fake_image, hr_image = model_NeuralNetwork(img_path+'/'+image)
 						img_array = tensor_to_array(tf.squeeze(fake_image))
-						img_output[c,:,:] = np.asarray(img_array)[:,:,0]			
+						updateIndex(metadata['channels']['index'], c)
+						img_output[inx0p:inx0,inx1p:inx1,inx2p:inx2] = np.asarray(img_array)[:,:,0]			
 			
 			if (img_output.ndim == 4):
-				for image in imgs:
-					c = int(image.split('.')[0].split('_')[0])-1
-					z = int(image.split('.')[0].split('_')[1])-1
-					it.printMessage('Processing: '+image+'\tc: '+str(c)+'\tz: '+str(z))
-					fake_image, hr_image = model_NeuralNetwork(img_path+'/'+image)
-					img_array = tensor_to_array(tf.squeeze(fake_image))
-					img_output[z,c,:,:] = np.asarray(img_array)[:,:,0]			
+				if(('channels' in metadata) and ('slices' in metadata)):
+					for image in imgs:
+						c = int(image.split('.')[0].split('_')[0])-1
+						z = int(image.split('.')[0].split('_')[1])-1
+						it.printMessage('Processing: '+image+'\tc: '+str(c)+'\tz: '+str(z))
+						fake_image, hr_image = model_NeuralNetwork(img_path+'/'+image)
+						img_array = tensor_to_array(tf.squeeze(fake_image))
+						updateIndex(metadata['channels']['index'], c)
+						updateIndex(metadata['slices']['index'], z)
+						img_output[inx0p:inx0,inx1p:inx1,inx2p:inx2,inx3p:inx3] = np.asarray(img_array)[:,:,0]
+				if(('channels' in metadata) and ('frames' in metadata)):
+					for image in imgs:
+						c = int(image.split('.')[0].split('_')[0])-1
+						f = int(image.split('.')[0].split('_')[1])-1
+						it.printMessage('Processing: '+image+'\tc: '+str(c)+'\tf: '+str(f))
+						fake_image, hr_image = model_NeuralNetwork(img_path+'/'+image)
+						img_array = tensor_to_array(tf.squeeze(fake_image))
+						updateIndex(metadata['channels']['index'], c)
+						updateIndex(metadata['frames']['index'], f)
+						img_output[inx0p:inx0,inx1p:inx1,inx2p:inx2,inx3p:inx3] = np.asarray(img_array)[:,:,0]
+				if(('frames' in metadata) and ('slices' in metadata)):
+					for image in imgs:
+						f = int(image.split('.')[0].split('_')[0])-1
+						z = int(image.split('.')[0].split('_')[1])-1
+						it.printMessage('Processing: '+image+'\tf: '+str(f)+'\tz: '+str(z))
+						fake_image, hr_image = model_NeuralNetwork(img_path+'/'+image)
+						img_array = tensor_to_array(tf.squeeze(fake_image))
+						updateIndex(metadata['frames']['index'], f)
+						updateIndex(metadata['slices']['index'], z)
+						img_output[inx0p:inx0,inx1p:inx1,inx2p:inx2,inx3p:inx3] = np.asarray(img_array)[:,:,0].reshape(img_output[inx0p:inx0,inx1p:inx1,inx2p:inx2,inx3p:inx3].shape)
 			
 			import src.interfaceTools as it
 			
 			(m,s) = it.getFormatTime(time.time() - start)
-			it.printMessage("Runtime: "+str(m)+" minutes, "+str(s)+" seconds")	
+			it.printMessage("Runtime: "+str(m)+" minutes, "+str(s)+" seconds")
 			
-			nnimg = it.NewWindow('Neural Network: '+it.windows_img[index].nameWindow, image = True)
+			if (it.windows_img[index].metadata['type']=='uint16'):
+				img_output = np.uint16(img_output*(4095/255))
+			
+			nnimg = it.NewWindow('Neural Network: '+it.windows_img[index].nameWindow, metadata=metadataimg, image = True)
 			if (display1f):
 				nnimg.placeImage(img_output)
 			else:	
@@ -233,3 +311,19 @@ def nn(img_tensor, index):
 				rmtree("src/cache/training_set")
 	else:
 		messagebox.showinfo(message='There is no input parameter')
+
+def updateIndex(index, pos):
+	global inx0, inx1, inx2, inx3, inx0p, inx1p, inx2p, inx3p
+
+	if (index==0):
+		inx0 = pos+1
+		inx0p = pos
+	if (index==1):
+		inx1 = pos+1
+		inx1p = pos
+	if (index==2):
+		inx2 = pos+1
+		inx2p = pos
+	if (index==3):
+		inx3 = pos+1
+		inx3p = pos			
